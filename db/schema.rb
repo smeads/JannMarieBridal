6f5d34893656a55b572d6c36b6cfb9e85e9b31c7
Steven Meads <sbmeads@gmail.com>
2016-09-02 15:48:06 -0700

    Added name to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902222823) do
+ActiveRecord::Schema.define(version: 20160902224707) do
 
   create_table "user_profiles", force: :cascade do |t|
     t.string   "first_name"
@@ -61,9 +61,11 @@ ActiveRecord::Schema.define(version: 20160902222823) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
