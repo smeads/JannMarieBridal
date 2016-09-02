b723a59b81ae6c6a7af51f2541bf1d2b7289efbb
Steven Meads <sbmeads@gmail.com>
2016-09-02 10:21:03 -0700

    updated new and create specs for user_profile

--- spec/controllers/user_profiles_controller_spec.rb
+++ spec/controllers/user_profiles_controller_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe UserProfilesController, type: :controller do
 
+let(:my_user_profile) { UserProfile.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+  email: Faker::Internet.email, phone_number: Faker::PhoneNumber.phone_number, shipping_address: Faker::Address.street_address,
+  wedding_date: Faker::Date.forward(23), bridals_date: Faker::Date.forward(20), height: Faker::Number.decimal(2),
+  over_bust: Faker::Number.decimal(2), bust: Faker::Number.decimal(2), under_bust: Faker::Number.decimal(2),
+  braw_size: Faker::Number.decimal(2), waist: Faker::Number.decimal(2), hip: Faker::Number.decimal(2),
+  natural_waist: Faker::Number.decimal(2), shoulder_to_shoulder: Faker::Number.decimal(2),
+  shoulder_to_waist: Faker::Number.decimal(2), waist_to_floor: Faker::Number.decimal(2),
+  rise: Faker::Number.decimal(2), sleeve_length: Faker::Number.decimal(2),
+  arm_hole: Faker::Number.decimal(2), bicep: Faker::Number.decimal(2),
+  forearm: Faker::Number.decimal(2), deposit: Faker::Boolean.boolean, paid_in_full: Faker::Boolean.boolean,
+  shipping: Faker::Boolean.boolean) }
+
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -20,12 +32,68 @@ RSpec.describe UserProfilesController, type: :controller do
   #   end
   # end
   #
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @user_profile" do
+      get :new
+      expect(assigns(:user_profile)).not_to be_nil
+    end
+  end
+
+  describe "POST create" do
+    it "increases the number of UserProfile by 1" do
+      expect{post :create, user_profile: {first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+        email: Faker::Internet.email, phone_number: Faker::PhoneNumber.phone_number, shipping_address: Faker::Address.street_address,
+        wedding_date: Faker::Date.forward(23), bridals_date: Faker::Date.forward(20), height: Faker::Number.decimal(2),
+        over_bust: Faker::Number.decimal(2), bust: Faker::Number.decimal(2), under_bust: Faker::Number.decimal(2),
+        braw_size: Faker::Number.decimal(2), waist: Faker::Number.decimal(2), hip: Faker::Number.decimal(2),
+        natural_waist: Faker::Number.decimal(2), shoulder_to_shoulder: Faker::Number.decimal(2),
+        shoulder_to_waist: Faker::Number.decimal(2), waist_to_floor: Faker::Number.decimal(2),
+        rise: Faker::Number.decimal(2), sleeve_length: Faker::Number.decimal(2),
+        arm_hole: Faker::Number.decimal(2), bicep: Faker::Number.decimal(2),
+        forearm: Faker::Number.decimal(2), deposit: Faker::Boolean.boolean, paid_in_full: Faker::Boolean.boolean,
+        shipping: Faker::Boolean.boolean}}.to change(UserProfile,:count).by(1)
+    end
+
+    it "assigns the new user_profile to @user_profile" do
+      post :create, user_profile: {first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+        email: Faker::Internet.email, phone_number: Faker::PhoneNumber.phone_number, shipping_address: Faker::Address.street_address,
+        wedding_date: Faker::Date.forward(23), bridals_date: Faker::Date.forward(20), height: Faker::Number.decimal(2),
+        over_bust: Faker::Number.decimal(2), bust: Faker::Number.decimal(2), under_bust: Faker::Number.decimal(2),
+        braw_size: Faker::Number.decimal(2), waist: Faker::Number.decimal(2), hip: Faker::Number.decimal(2),
+        natural_waist: Faker::Number.decimal(2), shoulder_to_shoulder: Faker::Number.decimal(2),
+        shoulder_to_waist: Faker::Number.decimal(2), waist_to_floor: Faker::Number.decimal(2),
+        rise: Faker::Number.decimal(2), sleeve_length: Faker::Number.decimal(2),
+        arm_hole: Faker::Number.decimal(2), bicep: Faker::Number.decimal(2),
+        forearm: Faker::Number.decimal(2), deposit: Faker::Boolean.boolean, paid_in_full: Faker::Boolean.boolean,
+        shipping: Faker::Boolean.boolean}
+        expect(assigns(:user_profile)).to eq UserProfile.last
+    end
+
+    it "redirects to the new user_profile" do
+      post :create, user_profile: {first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+        email: Faker::Internet.email, phone_number: Faker::PhoneNumber.phone_number, shipping_address: Faker::Address.street_address,
+        wedding_date: Faker::Date.forward(23), bridals_date: Faker::Date.forward(20), height: Faker::Number.decimal(2),
+        over_bust: Faker::Number.decimal(2), bust: Faker::Number.decimal(2), under_bust: Faker::Number.decimal(2),
+        braw_size: Faker::Number.decimal(2), waist: Faker::Number.decimal(2), hip: Faker::Number.decimal(2),
+        natural_waist: Faker::Number.decimal(2), shoulder_to_shoulder: Faker::Number.decimal(2),
+        shoulder_to_waist: Faker::Number.decimal(2), waist_to_floor: Faker::Number.decimal(2),
+        rise: Faker::Number.decimal(2), sleeve_length: Faker::Number.decimal(2),
+        arm_hole: Faker::Number.decimal(2), bicep: Faker::Number.decimal(2),
+        forearm: Faker::Number.decimal(2), deposit: Faker::Boolean.boolean, paid_in_full: Faker::Boolean.boolean,
+        shipping: Faker::Boolean.boolean}
+        expect(response).to redirect_to UserProfile.last
+    end
+  end
   #
   # describe "GET #edit" do
   #   it "returns http success" do
